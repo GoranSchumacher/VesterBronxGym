86af2bd27df72f6f8419af7cd908ddd2acccc718
GoranSchumacher <schumacher@me.com>
2015-01-18 21:23:48 +0100

    validarte birtDate.

--- app/models/UserProfile.java
+++ app/models/UserProfile.java
@@ -57,7 +57,7 @@ public class UserProfile extends Model{
             return "Angiv hvis vi må kontakte dej!";
         }
         if (birthDate == null ) {
-            birthDate = null;
+            return "Angiv fødselsdag!";
         }
         return null;
     }
